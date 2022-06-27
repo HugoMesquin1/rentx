ecc17febfa37558d5bbf23ee47b46be1329f176b
Hugo Vinicius Mesquini <hugomesq1n1@gmail.com>
2022-06-27 16:04:43 -0300

    Adicionando avatar a aplicação

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express"
+import uploadConfig from "../config/upload"
 import { CreateUserController } from "../modules/accounts/useCases/CreateUser/CreateUserController"
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController"
+import multer from "multer"
 
 
 const usersRoutes = Router()
 
 const createUserController = new CreateUserController()
+const updateUserAvatarController = new UpdateUserAvatarController()
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 
 usersRoutes.post("/", createUserController.handle)
+
+usersRoutes.patch("/avatar", uploadAvatar.single("Avatar") , updateUserAvatarController.handle)
 
 export {usersRoutes}
