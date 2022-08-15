7386113d250682f34bd33eab35e81bd145722638
Hugo Vinicius Mesquini <hugomesq1n1@gmail.com>
2022-08-15 12:30:23 -0300

    Refatoração de testes

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -26,21 +26,19 @@ describe("Create Car", () => {
     expect(car).toHaveProperty("id")
   })
 
-  it ("should be not able to create a car with exist license_plate", () =>{
-    expect (async () => {
-      await createCarUseCase.execute({
-        name: "Car1",
-        brand: "Brand",
-        description: "Description",
-        daily_rate: 100,
-        license_plate: "abc-123",
-        fine_amount: 60,
-        category_id: "category"
-      
-      
-    })
-
+  it ("should be not able to create a car with exist license_plate",async () =>{
     await createCarUseCase.execute({
+      name: "Car1",
+      brand: "Brand",
+      description: "Description",
+      daily_rate: 100,
+      license_plate: "abc-123",
+      fine_amount: 60,
+      category_id: "category"
+    
+    
+  })
+    await expect  (createCarUseCase.execute({
       name: "Car2",
       brand: "Brand",
       description: "Description",
@@ -49,7 +47,7 @@ describe("Create Car", () => {
       fine_amount: 60,
       category_id: "category"
     })
-   }).rejects.toBeInstanceOf(AppError)
+   ).rejects.toEqual(new AppError("Car Already Exists"))
  })
  
  it("should not be able to create a car with avaliable true by default", async () => {
